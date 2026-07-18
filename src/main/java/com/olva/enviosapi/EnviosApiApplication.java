a763c8e6c849b1168b06e75f1c2c416434d66193
CHOCEK <choceksalas@gmail.com>
2026-07-18 15:42:18 -0500

    chore: inicializacion de proyecto Spring Boot

---
+++ src/main/java/com/olva/enviosapi/EnviosApiApplication.java
@@ -0,0 +1,13 @@
+package com.olva.enviosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviosApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EnviosApiApplication.class, args);
+  }
+
+}
