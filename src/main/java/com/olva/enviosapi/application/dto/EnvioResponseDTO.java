c997f0971d15a692cd058855297a97f82c60bf9b
CHOCEK <choceksalas@gmail.com>
2026-07-18 19:42:33 -0500

    fix: agregar constructor vacio a EnvioResponseDTO

--- src/main/java/com/olva/enviosapi/application/dto/EnvioResponseDTO.java
+++ src/main/java/com/olva/enviosapi/application/dto/EnvioResponseDTO.java
@@ -1,12 +1,16 @@
 package com.olva.enviosapi.application.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EnvioResponseDTO {
   private String id;
   private String numeroTracking;
